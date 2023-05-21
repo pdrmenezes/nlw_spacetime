588ab7f5189ad4c65d9c10691fbc7e40e8e68aff
pdrmenezes <pdrmenezes1@gmail.com>
2023-05-21 20:40:40 -0300

    feat:auth middleware & redirect

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
 
-  // const redirectTo = request.cookies.get("redirectTo")?.value;
+  const redirectTo = request.cookies.get("redirectTo")?.value;
 
   const registerResponse = await api.post("/register", {
     code,
@@ -13,7 +13,7 @@ export async function GET(request: NextRequest) {
 
   const { token } = registerResponse.data;
 
-  const redirectURL = new URL("/", request.url);
+  const redirectURL = redirectTo ?? new URL("/", request.url);
 
   const oneMonthInSeconds = 2592000;
 
