5b8686109ee74b8868e1da97611c981850bb7a49
pdrmenezes <pdrmenezes1@gmail.com>
2023-05-21 18:45:16 -0300

    feat:setup de rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,6 +12,6 @@ app.register(jwt, { secret: "spacetime" });
 app.register(memoriesRoutes);
 app.register(authRoutes);
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running on: http://localhost:3333");
 });
