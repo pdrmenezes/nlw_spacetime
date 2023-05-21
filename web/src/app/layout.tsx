5e034e02b32d0d6273a2411d3b219c5855f76abf
pdrmenezes <pdrmenezes1@gmail.com>
2023-05-21 19:03:56 -0300

    feat:app layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,10 @@
+import { cookies } from "next/headers";
+import { Footer } from "@/components/Footer";
+import { Hero } from "@/components/Hero";
+import { Profile } from "@/components/Profile";
+import { SignIn } from "@/components/Signin";
 import "./globals.css";
 import { Space_Grotesk as SpaceGrotesk } from "next/font/google";
-
 const spaceGrotesk = SpaceGrotesk({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
@@ -18,12 +22,29 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isAuthenticated = cookies().has("token");
   return (
     <html lang="en">
       <body
         className={`${spaceGrotesk.variable} bg-spacetime_gray-900 font-sans text-spacetime_gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* left column */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* blur bg effect */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-spacetime_purple-700 opacity-50 blur-full" />
+            {/* timeline */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Footer />
+          </div>
+
+          {/* right column */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,31 +1,5 @@
-import { cookies } from "next/headers";
-
-import { SignIn } from "@/components/Signin";
-import { Hero } from "@/components/Hero";
-import { Footer } from "@/components/Footer";
 import { EmptyMemories } from "@/components/EmptyMemories";
-import { Profile } from "@/components/Profile";
 
 export default function Home() {
-  const isAuthenticated = cookies().has("token");
-
-  return (
-    <main className="grid min-h-screen grid-cols-2">
-      {/* left column */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-        {/* blur bg effect */}
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-spacetime_purple-700 opacity-50 blur-full" />
-        {/* timeline */}
-        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
-        {isAuthenticated ? <Profile /> : <SignIn />}
-        <Hero />
-        <Footer />
-      </div>
-
-      {/* right column */}
-      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <EmptyMemories />
-      </div>
-    </main>
-  );
+  return <EmptyMemories />;
 }
